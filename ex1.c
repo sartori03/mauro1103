366be5f40e642268fff3118352b3d9b591b1fbf4
sartori03 <marcolinosartori03@gmail.com>
2025-03-11 20:16:20 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int IMC;
+
+  printf("Digite o IMC: ");
+  scanf("%d", &IMC);
+
+  if( IMC >= 18.5 )
+  if( IMC <= 24.9) {
+     printf("Dentro do normal!");
+    } else {
+      printf("Fora do normal!");
+    } else {
+      printf("Fora do normal!");
+    }
+     return 0;
+     }
+ 
